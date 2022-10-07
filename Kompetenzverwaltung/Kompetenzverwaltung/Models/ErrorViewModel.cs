ccfcb7a138c0c255bef3b23c9b247027ce98a336
Marcel Steiner <marcelsteiner@bluewin.ch>
2022-10-07 14:41:09 +0200

    Add Project Structure

---
+++ Kompetenzverwaltung/Kompetenzverwaltung/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kompetenzverwaltung.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
