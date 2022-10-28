192d66ae5aac0a27f5502e2074195c16ae0708d0
Tim Arnold <tarnold@online.gibz.ch>
2022-10-28 14:17:02 +0200

    Move Connectionstring to Businesslayer

---
+++ Kompetenzverwaltung/BL/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BL.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: "Server=.\\SQLEXPRESS;Database=M326_Kompetenzverwaltung;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
